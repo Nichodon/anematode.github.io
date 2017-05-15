5593c681d95a055db3d8bfec888743efd7f667c3
anematode <timothy.herchen@gmail.com>
2017-05-14 22:14:05 -0700

    Added footer.js, nav.js, modified index, about, added biographies, added portraits

---
+++ footer.js
@@ -0,0 +1,33 @@
+document.getElementById("footerPlaceholder").innerHTML =
+'<!-- Section One -->'+
+'<section class="one_third">'+
+'<h2 class="title">FTC Information</h2>'+
+'<nav>'+
+'<ul>'+
+'<li><a href="https://www.firstinspires.org/robotics/ftc">Official FTC Website</a></li>'+
+'<li><a href="#">Our FTC Experience</a></li>'+
+'<li><a href="#">Link 3</a></li>'+
+'<li><a href="#">Link 4</a></li>'+
+'<li class="last"><a href="#">Link 5</a></li>'+
+'</ul>'+
+'</nav>'+
+'</section>'+
+'<!-- Section Two -->'+
+'<section class="one_third">'+
+'<h2 class="title">Topic 2</h2>'+
+'<nav>'+
+'<ul>'+
+'<li><a href="#">Link 1</a></li>'+
+'<li><a href="#">Link 2</a></li>'+
+'<li><a href="#">Link 3</a></li>'+
+'<li><a href="#">Link 4</a></li>'+
+'<li class="last"><a href="#">Link 5</a></li>'+
+'</ul>'+
+'</nav>'+
+'</section>'+
+'<!-- Section Three -->'+
+'<section class="one_third lastbox">'+
+'<h2 class="title">Contact Us</h2>'+
+'<p>Contact Information</p>'+
+'</section>'+
+'<!-- / Section -->'
---
+++ nav.js
@@ -0,0 +1,15 @@
+document.getElementById("navMenu").innerHTML =
+'<div id="hgroup">'+
+    '<h1><a href="#">FTC Team 11101</a></h1>'+
+    '<h2>Robot Fanatics</h2>'+
+'</div>'+
+'<nav>'+
+    '<ul>'+
+        '<li><a href="index.html">Home</a></li>'+
+        '<li><a href="#">2016 Season</a></li>'+
+        '<li><a href="#">2017 Season</a></li>'+
+        '<li><a href="#">Our Robots</a></li>'+
+        '<li><a href="#">FTC Info</a></li>'+
+        '<li class="last"><a href="about.html">About Us</a></li>'+
+    '</ul>'+
+'</nav>'
