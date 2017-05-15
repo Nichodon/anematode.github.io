847620084564e3c0c9519fe9ee19121d77e0a53c
anematode <timothy.herchen@gmail.com>
2017-05-15 07:24:43 -0700

    Added personal math blog (Grant, do not touch)

---
+++ math/nav.js
@@ -0,0 +1,15 @@
+document.getElementById("navMenu").innerHTML =
+'<div id="hgroup">'+
+    '<h1><a href="#">Visual Math</a></h1>'+
+    '<h2>A Lovely Math Blog</h2>'+
+'</div>'+
+'<nav>'+
+    '<ul>'+
+        '<li><a href="index.html">Home</a></li>'+
+        '<li><a href="#">Fractals</a></li>'+
+        '<li><a href="#">Programming</a></li>'+
+        '<li><a href="#">Other Math</a></li>'+
+        '<li><a href="#">Calculators</a></li>'+
+        '<li class="last"><a href="about.html">Math Worksheets</a></li>'+
+    '</ul>'+
+'</nav>'
