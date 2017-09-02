13e056e26c1c4a9336715b3c30b70f9047adc248
Nichodon <brandonsychung@gmail.com>
2017-09-02 09:54:02 -0700

    add about page

--- frame.js
+++ frame.js
@@ -4,3 +4,11 @@ document.getElementById("navbar").innerHTML =
 	'<a>Robots</a>' +
 	'<a>About</a>' +
 	'<a>Contribute</a>';
+
+var parallax = document.getElementsByClassName("parallax")[0];
+
+window.onscroll = function() {
+	"use strict";
+	var rate = document.body.scrollTop * 25 / parallax.offsetHeight;
+	parallax.style.transform = "translate(0%, " + rate + "%)";
+};
