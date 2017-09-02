7db78b281556bd11e543f73910dedbe4115b6048
Nichodon <brandonsychung@gmail.com>
2017-09-02 10:35:54 -0700

    add dropdown

--- frame.js
+++ frame.js
@@ -1,11 +1,36 @@
-document.getElementById("navbar").innerHTML =
+var navbar = document.getElementById("navbar");
+
+navbar.innerHTML =
+	'<div>' +
 	'<a>Home</a>' +
-	'<a>Seasons</a>' +
+	'<a id="d1">Seasons &#9662;</a>' +
 	'<a>Robots</a>' +
 	'<a>About</a>' +
-	'<a>Contribute</a>';
+	'<a>Contribute</a>' +
+	'</div>' + 
+	'<div>' +
+	'<a id="d2">&#9652;</a>' +
+	'<a>2016 &ndash; 2017</a>' +
+	'<a>2017 &ndash; 2018</a>' +
+	'<a>2018 &ndash; 2019</a>' +
+	'</div>';
 
-var parallax = document.getElementsByClassName("parallax")[0];
+navbar.children[0].style.position = "absolute";
+navbar.children[1].style.transform = "translate(0, 100%)";
+
+document.getElementById("d1").onclick = function() {
+	"use strict";
+	navbar.children[0].style.transform = "translate(0, -200%)";
+	navbar.children[1].style.transform = "translate(0, 0)";
+};
+
+document.getElementById("d2").onclick = function() {
+	"use strict";
+	navbar.children[0].style.transform = "translate(0, 0)";
+	navbar.children[1].style.transform = "translate(0, 200%)";
+};
+
+var parallax = document.getElementById("parallax");
 
 window.onscroll = function() {
 	"use strict";
